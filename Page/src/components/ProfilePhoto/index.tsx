4dc0b80b835d17dae1746506038c20df06e69e48
jhonataT <dev.tenorio.jhonata@gmail.com>
2024-08-03 02:08:58 -0300

    chore: add component test <profilePhoto>

--- Page/src/components/ProfilePhoto/index.tsx
+++ Page/src/components/ProfilePhoto/index.tsx
@@ -6,10 +6,12 @@ interface ProfilePhotoProps {
 
 export const ProfilePhoto = ({ name }: ProfilePhotoProps) => {
   let formattedName: string[] | string = name.split(' ');
-  formattedName = `${formattedName[0][0].toUpperCase() || 'U'}${formattedName[1][1].toUpperCase() || ''}`;
+  formattedName = 
+    `${formattedName[0] ? formattedName[0][0].toUpperCase() : 'U'}`
+    +`${formattedName[1] ? formattedName[1][0].toUpperCase() : ''}`;
 
-  return <ProfileContainer>
-    <span>
+  return <ProfileContainer data-testid="profile-container">
+    <span data-testid="profile-name">
       {formattedName}
     </span>
   </ProfileContainer>
