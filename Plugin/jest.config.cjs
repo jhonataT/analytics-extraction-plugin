7691030c30d9e872eb89df22387c0bbfbd72d619
jhonataT <dev.tenorio.jhonata@gmail.com>
2024-08-04 15:34:13 -0300

    chore: finalizing unity tests rules

---
+++ Plugin/jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  setupFiles: ['./test/jest.setup.ts'],
+};
